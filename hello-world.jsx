d9c0ff4454b7950dbd867066d79fa73290b82863
John M-W <john@miller-williams.name>
2021-09-21 20:33:36 +1200

    Testing updated response

--- hello-world.jsx
+++ hello-world.jsx
@@ -11,7 +11,7 @@ function App() {
         <link rel="stylesheet" href="style.css" />
       </head>
       <body>
-        <h1>Hello world from Deno Deploy via Github - testing responses</h1>
+        <h1>Hello world from Deno Deploy via Github - Testing Deploy with Response edits</h1>
       </body>
     </html>
   );
