6eae14429d5562f0808db496124bf7b5bbd7506c
John M-W <john@miller-williams.name>
2021-09-21 21:01:22 +1200

    Trying static assets again

--- hello-world.tsx
+++ hello-world.tsx
@@ -18,6 +18,25 @@ function App() {
 }
 
 async function handleRequest(request: Request) {
+  const { pathname } = new URL(request.url);
+
+  // This is how the server works:
+  // 1. A request comes in for a specific asset.
+  // 2. We read the asset from the file system.
+  // 3. We send the asset back to the client.
+
+  // Check if the request is for style.css.
+  if (pathname.startsWith("/style.css")) {
+    // Read the style.css file from the file system.
+    const file = await Deno.readFile("./style.css");
+    // Respond to the request with the style.css file.
+    return new Response(file, {
+      headers: {
+        "content-type": "text/css",
+      },
+    });
+  }
+  
   const response = new Response(renderToString(<App />), {
      headers: { "content-type": "text/html; charset=utf-8" },
   });
